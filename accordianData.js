f7c681f98f41947d5264eb28066e33a284713479
Faseela <faseela8861@gmail.com>
2025-03-27 12:22:52 +0530

    Add existing project into github

---
+++ accordianData.js
@@ -0,0 +1,22 @@
+const accordianData =[
+    {
+        id:100,
+        title:"What is accordian format?",
+        desc : "Accordion format is a web design pattern that uses a vertically stacked list of headers to reveal or hide content",
+
+    },
+    {
+        id:101,
+        title:"What is accordian item?",
+        desc : "An accordion item refers to a single, expandable section within an accordion component on a website",
+
+    },
+    {
+        id:102,
+        title:"What is accordian text?",
+        desc : "Accordion text is the text that appears in the summary line of each section in an accordion",
+
+    },
+    
+]
+export default accordianData
